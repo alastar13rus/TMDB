16571a86c4f82f9634a796ba5b970e860ac220f8
Dokin Andrei <alastar13rus@gmail.com>
2021-04-22 01:28:14 +0300

    Добавлена сцена PeopleDetail      - секция с профилем      - секция с биографией (режимы short и full)      - секция "Лучшие работы"          - 5 фильмов + 5 сериалов, отобранных по рейтингу

---
+++ TMDB/TMDB/Scene/PeopleDetail/View/Cell/PeopleBestMediaListTableViewCell.swift
@@ -0,0 +1,73 @@
+//
+//  PeopleBestMediaListTableViewCell.swift
+//  TMDB
+//
+//  Created by Докин Андрей (IOS) on 18.04.2021.
+//
+
+import UIKit
+import RxSwift
+import RxDataSources
+
+class PeopleBestMediaListTableViewCell: UITableViewCell {
+    
+//    MARK: - Properties
+    var viewModel: PeopleBestMediaListViewModel! {
+        didSet {
+            configure(with: viewModel)
+        }
+    }
+    
+    let dataSource = BestCreditInMediaListDataSource.dataSource()
+    let disposeBag = DisposeBag()
+    
+    lazy var creditInMediaListCollectionView: UICollectionView = {
+        let layout = CollectionViewLayout(countItemsInRowOrColumn: 3, scrollDirection: .horizontal, view: self)
+        layout.headerReferenceSize.width = 50
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(CreditInMediaCell.self, forCellWithReuseIdentifier: String(describing: CreditInMediaCell.self))
+        collectionView.register(ReusableHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: String(describing: ReusableHeaderView.self))
+        collectionView.backgroundColor = .white
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    
+//    MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+        setupHierarhy()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+//    MARK: - Methods
+    fileprivate func configure(with vm: PeopleBestMediaListViewModel) {
+        viewModel.sectionedItems.asDriver(onErrorJustReturn: []).drive(creditInMediaListCollectionView.rx.items(dataSource: dataSource)).disposed(by: disposeBag)
+    }
+    
+    fileprivate func setupUI() {
+        
+    }
+    
+    fileprivate func setupHierarhy() {
+        contentView.addSubview(creditInMediaListCollectionView)
+    }
+    
+    fileprivate func setupConstraints() {
+        NSLayoutConstraint.activate([
+            creditInMediaListCollectionView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            creditInMediaListCollectionView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            creditInMediaListCollectionView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            creditInMediaListCollectionView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+        ])
+    }
+}
---
+++ TMDB/TMDB/Scene/PeopleDetail/ViewModel/CellViewModel/PeopleProfileWrapperCellViewModel.swift
@@ -0,0 +1,81 @@
+//
+//  PeopleProfileWrapperCellViewModel.swift
+//  TMDB
+//
+//  Created by Докин Андрей (IOS) on 18.04.2021.
+//
+
+import Foundation
+
+struct PeopleProfileWrapperCellViewModel {
+    
+//    MARK: - Properties
+    let id: String
+    let name: String
+    let placeOfBirth: String?
+    let birthday: String?
+    let deathday: String?
+    let castList: [CastInMediaModel]?
+    let crewList: [CrewInMediaModel]?
+    
+    let profilePath: String?
+    
+    var placeAndBirthday: String {
+        switch (birthday, placeOfBirth) {
+        case (.some(let birthday), .some(let placeOfBirth)):
+            return toRussianDate(from: birthday) + ", " + placeOfBirth
+        case (.some(let birthday), .none):
+            return toRussianDate(from: birthday)
+        case (.none, .some(let placeOfBirth)):
+            return placeOfBirth
+        case (.none, .none):
+            return ""
+        }
+    }
+    
+    var job: String {
+        var job = [String]()
+        guard let castList = castList, let crewList = crewList else { return "" }
+        if !castList.isEmpty { job.append("Actor") }
+        job.append(contentsOf: Array(Set(crewList.map { $0.job })))
+        return job.joined(separator: ", ")
+    }
+    
+    
+    func profileImageData(completion: @escaping (Data?) -> Void) {
+        guard let profilePath = profilePath else { completion(nil); return }
+        
+        guard let profileAbsoluteURL = ImageURL.profile(.w185, profilePath).fullURL else { completion(nil); return }
+        
+        profileAbsoluteURL.downloadImageData { (imageData) in
+            completion(imageData)
+        }
+        
+    }
+    
+    
+//    MARK: - Init
+    init(_ model: PeopleDetailModel) {
+        self.id = "\(model.id)"
+        self.name = model.name
+        self.placeOfBirth = model.placeOfBirth
+        self.birthday = model.birthday
+        self.deathday = model.deathday
+        self.profilePath = model.profilePath
+        self.castList = model.combinedCredits?.cast
+        self.crewList = model.combinedCredits?.crew
+    }
+    
+    fileprivate func toRussianDate(from dateString: String) -> String {
+        let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        guard let date = dateFormatter.date(from: dateString) else { return "" }
+        
+        let dateFormatter2 = DateFormatter()
+            dateFormatter2.dateFormat = "dd.MM.yyyy"
+        let dateString = dateFormatter2.string(from: date)
+        
+        return dateString
+    }
+}
